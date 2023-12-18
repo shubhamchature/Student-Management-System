9a6da66474b2cf9c100670278ae67c634520a98f
Shubham Chature <148949453+shubhamchature@users.noreply.github.com>
2023-12-18 17:12:21 +0530

    Update helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://sms-backend-app.azurewebsites.net/"
+export const BASE_URL = "https://sms-backend-app.azurewebsites.net"
 // export const BASE_URL = "https://mernappbackend.onrender.com"
